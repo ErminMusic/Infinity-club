c592fee1517b2d72142494bb1397c2b09a0216df
Ermin Music <emusic444@gmail.com>
2022-11-09 13:51:27 +0100

    MenuList Props

--- pages/components/SmallComponents/Menu/MenuList.js
+++ pages/components/SmallComponents/Menu/MenuList.js
@@ -1,14 +1,14 @@
-function MenuList( { drinkName, drinkList } ) {
+function MenuList(props) {
   return (
     <div data-aos="fade-up" data-aos-duration="1500" 
     className=" flex flex-wrap flex-col
     p-1 py-2 pr-5 w-1/2 max75:w-full">
 
       <div className="text-white text-lg font-bold mb-4" >
-        {drinkName}
+        {props.drinkName}
       </div>
   
-      {drinkList.map((item, index) => (
+      {props.drinkList.map((item, index) => (
 
         <div className=" 
         flex justify-between py-1
