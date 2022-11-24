d574b4f94ef246ef28e3562afea97e97c34bb455
Ermin Music <emusic444@gmail.com>
2022-11-24 08:16:27 +0100

    Button fix

--- pages/components/SmallComponents/Welcome/Buttons.js
+++ pages/components/SmallComponents/Welcome/Buttons.js
@@ -8,18 +8,18 @@ function Buttons() {
       className="border border-cus-yellow text-cus-yellow hover:bg-cus-yellow/60
       transition-all hover:text-black bg-black/50 py-2 px-10 w-36 cursor-pointer
       flex flex-col justify-center items-center max45:my-2 max75:mt-2 whitespace-nowrap	
-      duration-500" 
+      duration-500 text-lg font-medium " 
       > Find Us</div>
 
       <div 
       className="border border-cus-yellow text-cus-yellow hover:bg-cus-yellow/60
       transition-all hover:text-black bg-black/80 py-2 px-10 w-36 cursor-pointer
       flex flex-col justify-center items-center max45:my-2 max75:mt-2 whitespace-nowrap	
-      duration-500" 
+      duration-500 text-lg font-medium " 
       > Our Menu</div>
 
     </div>
-    )
+  )
 }
 
 export default Buttons
