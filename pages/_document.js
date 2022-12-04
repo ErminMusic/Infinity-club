b817c55576933adc74197dfe6684f6ff488493af
Ermin Music <emusic444@gmail.com>
2022-12-04 09:45:00 +0100

    Google Tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,35 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <Script
+          id='google tags manager'
+          dangerouslySetInnerHTML={{
+          __html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5ZBS58T');`,
+          }}
+        />
+      </Head>
       <body>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5ZBS58T"
+          height="0" width="0" style="display:none;visibility:hidden" />`,
+        }}
+        />
+    
         <Main />
         <NextScript />
         <script
-            async src={`https://www.googlemanager.com/gtag/js?id=${GA_TRACKING_ID}`} 
+          async src={`https://www.googlemanager.com/gtag/js?id=${GA_TRACKING_ID}`} 
         />
         <script
           dangerouslySetInnerHTML={{
